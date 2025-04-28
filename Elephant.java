961931049c3b835ed974657b7186aea882fb3605
senaG1 <161520172+senaG1@users.noreply.github.com>
2025-04-28 16:00:51 -0400

    Made elephant move and apple drop. Elephant can eat apple

---
+++ Elephant.java
@@ -0,0 +1,39 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The Ele[hant, our hero
+ * 
+ * @author Sena Godek 
+ * @version April 28 2025
+ */
+public class Elephant extends Actor
+{
+    /**
+     * Act - do whatever the Elephant wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        if(Greenfoot.isKeyDown("left")){
+            move(-2);
+        }
+        else if(Greenfoot.isKeyDown("right")){
+            move(2);
+        }
+        
+        //remove apple of elephant eatsit
+        eat();
+    }
+    /**
+    * Eats the apple and spawn new apple if an apple is eaten
+    */
+    public void eat(){
+        if(isTouching(Apple.class)){
+            removeTouching(Apple.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.createApple();
+            world.increaseScore();
+        }    
+    }
+}
