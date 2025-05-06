4637b2f26019788dffb0f6a6397a17669b8af120
senaG1 <161520172+senaG1@users.noreply.github.com>
2025-05-06 18:13:23 -0400

    Changed and set apple image

--- Apple.java
+++ Apple.java
@@ -12,6 +12,10 @@ public class Apple extends Actor
      * Act - do whatever the Apple wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Apple()
+    {
+        setImage("images/apple.png");
+    }
     
     public void act()
     {
