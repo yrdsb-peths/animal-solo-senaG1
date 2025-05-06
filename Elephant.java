fe140273c1afd1dd8b7b1a93b7e2268b85ad7ac8
senaG1 <161520172+senaG1@users.noreply.github.com>
2025-05-06 17:46:55 -0400

    Created an array of the elephant images

--- Elephant.java
+++ Elephant.java
@@ -9,15 +9,23 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
-    GreenfootImage idle = new GreenfootImage("images/elephant_idle/idle0.png");
+    GreenfootImage[] idle = new GreenfootImage[8];
     
     /**
     * Constructor - The code that gets run   
     */
     public Elephant()
     {
-        setImage(idle);
+        for(int i = 0; i < idle.length; i++)
+        {
+            idle[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+        }
+        setImage(idle[0]);
     }
+    
+    /**
+    * Animate the elephant   
+    */
     
     public void act()
     {
